2c2e6af3870fbd569ee2584c709d68dddb8b030f
robi <robika.beglarashvili@mail.ru>
2025-02-04 11:34:36 +0400

    update store ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,20 @@
+// import { legacy_createStore as createStore, applyMiddleware } from "redux";
+// import { thunk } from "redux-thunk";
+
+// import rootReducer from "./reducers";
+
+// const store = createStore(rootReducer, applyMiddleware(thunk));
+
+// export default store;
+
+// export type RootState = ReturnType<typeof rootReducer>;
+// export type AppDispatch = typeof store.dispatch;
+
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import { thunk } from "redux-thunk";
-
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, undefined, applyMiddleware(thunk));
 
 export default store;
 
